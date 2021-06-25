711f857f797b8658293ab95fa6cad48013ac7ce7
Ashutosh Muley <muleyashutosh@gmail.com>
2021-06-25 10:52:36 +0530

    publicUrl domain change

--- resources/user/user.model.js
+++ resources/user/user.model.js
@@ -187,7 +187,7 @@ UserSchema.pre("save", async function (next) {
     this.lastName
   );
 
-  this.publicUrl = `https://konsult-member.com/${this.username}`;
+  this.publicUrl = `https://kourse-53d4f.web.app/#/public/${this.username}`;
 
   const params = {
     name: `${this.firstName} ${this.lastName}`,
