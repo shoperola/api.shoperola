09f9db41c2ee967bc661bbc188de35e51c00d516
Ashutosh Muley <muleyashutosh@gmail.com>
2021-07-12 18:12:43 +0530

    product model + controllers + cognito id changed

--- resources/Ecommerce/Ecommerce_controller.js
+++ resources/Ecommerce/Ecommerce_controller.js
@@ -1,2 +1,94 @@
-import {Ecommerce} from "./ecomerce_model";
+import { Ecommerce } from "./ecomerce_model";
 
+const getProducts = async (req, res) => {
+  try {
+    if (!req.user) {
+      return res.status(400).json({ message: "User Not Found" });
+    }
+    const products = await Ecommerce.find({ userID: req.user._id });
+    res.json({ status: "OK", data: products });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({ message: "Error getting products" });
+  }
+};
+
+const getProductById = async (req, res) => {
+  try {
+    if (!req.user) {
+      return res.status(400).json({ message: "User Not Found" });
+    }
+    const id = req.params.id;
+    if (!id) {
+      return res.status(400).json({ message: "id is required" });
+    }
+    const product = await Ecommerce.findById(id);
+    console.log(product);
+    res.json({ status: "OK", data: product });
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).json({ message: "Error getting product" });
+  }
+};
+
+const addProduct = async (req, res) => {
+  try {
+    if (!req.user) {
+      return res.status(400).json({ message: "User Not Found" });
+    }
+    const image = req.file;
+    const updateObject = image
+      ? { ...req.body, image: image.location }
+      : req.body;
+    const product = await Ecommerce.create(updateObject);
+    res.json({ status: "OK", data: product });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({ message: "Error Creating Product" });
+  }
+};
+
+const updateProduct = async (req, res) => {
+  try {
+    if (!req.user) {
+      return res.status(400).json({ message: "User Not Found" });
+    }
+    const id = req.params.id;
+    if (!id) {
+      return res.status(400).json({ message: "id is required" });
+    }
+    const image = req.file;
+    const updateObject = image
+      ? { ...req.body, image: image.location }
+      : req.body;
+    const product = await Ecommerce.findByIdAndUpdate(id, updateObject, {
+      new: true,
+    });
+    res.json({ status: "OK", data: product });
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).json({ message: "Error updating product" });
+  }
+};
+
+const deleteProduct = async (req, res) => {
+  try {
+    const id = req.params.id;
+    if (!id) {
+      return res.status(400).json({ message: "id not required" });
+    }
+    await Ecommerce.findByIdAndDelete(id);
+    res.json({ status: "OK", message: "Product Deleted Successfully" });
+  } catch (e) {
+    console.log(e.message);
+    res.json({ message: "Error deleting product" });
+  }
+};
+
+export {
+  getProducts,
+  getProductById,
+  addProduct,
+  updateProduct,
+  deleteProduct,
+};
--- resources/Ecommerce/ecomerce_model.js
+++ resources/Ecommerce/ecomerce_model.js
@@ -4,15 +4,17 @@ const { Schema, model, SchemaTypes } = mongoose;
 
 const EcommerceSchema = new Schema(
   {
-    title:{type:String},
-    description:{type:String},
-    image:{type:String},
-    price:{type:Number},
-    sale_price:{type:Number},
-    sku:{type:String},
-    quantity:{type:Number},
-    category:{ type: SchemaTypes.ObjectId,
-        ref: "Category",}
+    title: String,
+    description: String,
+    image: String,
+    price: Number,
+    sale_price: Number,
+    sku: String,
+    quantity: Number,
+    track_quantity: Boolean,
+    continue_selling: Boolean,
+    category: { type: SchemaTypes.ObjectId, ref: "Category" },
+    userID: { type: SchemaTypes.ObjectId, ref: "users" },
   },
   { timestamps: true }
 );
