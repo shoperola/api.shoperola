f3e2cd51b38b66e81cddaa8036ec3c4592ec6f22
Medusa <kawthekar56@gmail.com>
2021-07-13 13:31:55 +0530

    - deploy

--- resources/Cart/cart_contoller.js
+++ resources/Cart/cart_contoller.js
@@ -1,33 +1,49 @@
-// import {Cart} from "./cart_model";
-// import {Client} from "../client/client.model";
+import {Cart} from "./cart_model";
+import {Client} from "../client/client.model";
 
-// const update_cart = async (req, res) => {
-//     try{
-//         if(!req.user){
-//             return res.status(400).json({ message: "User Not Found" });
-//         }
-//         const client = await Client.findOne({ sub: req.user.sub });
-//         const cart = await Cart.findByIdAndUpdate(client.cartid,{$addToSet: {products: req.body.productid}} ,{new:true});
-//         res.send(cart)
-//     }catch(e){
-//         res.send(e)
-//     }
-// };
+const update_cart = async (req, res) => {
+    try{
+        if(!req.user){
+            return res.status(400).json({ message: "User Not Found" });
+        }
+        const client = await Client.findOne({ sub: req.user.sub });
+        
+        const cart = await Cart.findByIdAndUpdate(client.cartid,{$addToSet: {products: req.body.productid}} ,{new:true});
+        res.send(cart)
+    }catch(e){
+        res.send(e)
+    }
+};
 
-// const view_cart = async (req, res) => {
-//     try{
-//         if(!req.user){
-//             return res.status(400).json({ message: "User Not Found" });
-//         }
-//         const check = await Cart.find({})
-//         res.status(200).send(check) 
-//     }catch(e){
-//         res.send(e);
-//     }
-// };
+const view_cart = async (req, res) => {
+    try{
+        if(!req.user){
+            return res.status(400).json({ message: "User Not Found" });
+        }
+        const client = await Client.findOne({ sub: req.user.sub });
+        const check = await Cart.findOne({})
+        res.status(200).send(check) 
+    }catch(e){
+        res.send(e);
+    }
+};
 
-// const remove_product = async (req, res) => {
-//     try{
-//     }
-// }
+const remove_product = async (req, res) => {
+    try{
+        if(!req.user){
+            return res.status(400).json({ message: "User Not Found" });
+        }
+        const client = await Client.findOne({ sub: req.user.sub });
+    
+    const id = req.params.id;
+    const remove = await Cart.findByIdAndUpdate(client.cartid,{$pull: {products: id}});
+    res.send(remove);
+    }catch(e){
+        res.send(e)
+    }
+
+};
+
+
+export {update_cart,view_cart,remove_product}
 
---
+++ resources/Cart/cart_routes.js
@@ -0,0 +1,17 @@
+import { Router } from "express";
+
+import {update_cart,view_cart,remove_product} from "./cart_contoller";
+const router = Router();
+
+router.post("/add_product", update_cart);
+router.get("/view_cart", view_cart);
+router.delete("/remove_product", remove_product);
+
+
+
+
+
+
+
+
+export default router;
