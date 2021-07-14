94e193aedfdf3e317c1dae704d5fa584d40d0749
Ashutosh Muley <muleyashutosh@gmail.com>
2021-07-14 15:58:57 +0530

    firebase auth testing

--- util/firebase.js
+++ util/firebase.js
@@ -1,7 +1,7 @@
 import admin from "firebase-admin";
 import serviceAccount from "../tellytell-236a3-firebase-adminsdk-5j5og-9217d9f856.json";
 
-// console.log(serviceAccount);
+console.log(serviceAccount);
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
@@ -12,7 +12,9 @@ export const firebaseAuthProtect = async (req, res, next) => {
     if (!Authorization) {
       return res.status(401).json({ message: "Token Not Provided" });
     }
+    console.log(Authorization);
     const token = Authorization.split("Bearer ")[1].trim();
+    console.log(token);
     if (!token) {
       return res.status(401).json({ message: "Token Not Provided" });
     }
