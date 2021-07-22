76d047fc22f5aece3e96e2958eabbcbbc6236939
Medusa <kawthekar56@gmail.com>
2021-07-22 16:49:28 +0530

    - bug fixed

--- resources/Studio/studio_routes.js
+++ resources/Studio/studio_routes.js
@@ -7,7 +7,7 @@ import {
 } from "./studio_contoller";
 const router = Router();
 
-router.post("/add_product_list", add_product);
+router.post("/add_product_list/:id", add_product);
 router.get("/view_product", view_list);
 router.get("/view_byid/:id", view_listbyid);
 router.delete("/delete_product/:id", delete_product);
