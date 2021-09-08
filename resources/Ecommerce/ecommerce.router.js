55218e4826d1b5260c3ae9b1e26502f998bdb3f7
Tazim <tazimmadre5041@gmail.com>
2021-09-08 11:32:53 +0530

    Add product bug fixed

--- resources/Ecommerce/ecommerce.router.js
+++ resources/Ecommerce/ecommerce.router.js
@@ -9,15 +9,22 @@ import {
 } from "./Ecommerce_controller";
 import {add_tax} from "../zero_tax/zero_controller";
 import { upload } from "../../util/s3-spaces";
-
+const uploadFields = [
+  { name: "image" },
+  { name: "image1" },
+  { name: "image2" },
+  { name: "image3" },
+  { name: "image4" },
+  { name: "image5" }
+];
 const router = Router();
 
-router.route("/").get(getProducts).post(upload.single("image"), addProduct);
+router.route("/").get(getProducts).post(upload.fields(uploadFields), addProduct);
 
 router
   .route("/:id")
   .get(getProductById)
-  .put(upload.single("image"), updateProduct)
+  .put(upload.fields(uploadFields), updateProduct)
   .delete(deleteProduct);
 router.post('/count_delete_tax/:id', count_product);
 router.post('/add_tax', add_tax);
