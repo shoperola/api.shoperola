fedb9d9b104edb10062295e5043c39c05da74690
Medusa <kawthekar56@gmail.com>
2021-09-14 12:06:04 +0530

    'heroku'

---
+++ resources/variants/variant_model.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+const { Schema, SchemaTypes, model } = mongoose;
+
+const VariantSchema = Schema({
+  title: { type: String},
+  variant_title: [{ type: String}],
+  variant_price: [{type: Number}],
+  variant_quantity: [{ type: Number}],
+  variant_sku: [{ type:String}]
+  //userID: { type: SchemaTypes.ObjectId, ref: "users" },
+  
+
+});
+
+export const Variants = model("Variants", VariantSchema);
